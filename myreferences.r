82775de7bd2ee3bc97cdda032b768cb30efd8d65
Andrea Termine <43490939+Andreater@users.noreply.github.com>
2021-10-06 10:47:07 +0200

    Delete myreferences.r

--- myreferences.r
+++
@@ -1,3 +0,0 @@
-ref <- readRDS("/home/lab401/Desktop/Bioinf_job/Analyses/Tool AMD/data/reference/genetic_risk.rds")
-df <- data.frame(gene = unique(ref$gene), selected_genotype = NA, or.norm = NA)
-combs <- readxl::read_xlsx("/home/lab401/Analyses/Tool AMD/data/reference/combinations.xlsx")
